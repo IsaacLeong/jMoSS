739902bafe38b1c8cbe51fc03e2ea783f2de362b
IsaacLeong <leongshunking@gmail.com>
2018-04-27 21:51:56 +1000

    Updated Search Menu
    Added search movie functionality

--- jMoSS/jMoSS/src/model/menus/SearchMenu.java
+++ jMoSS/jMoSS/src/model/menus/SearchMenu.java
@@ -13,7 +13,20 @@ public class SearchMenu implements MenuInterface {
 		 * In the case of sub menus for a section, consider the use switches, and a storage variable
 		 * for user input selection
 		 */
-		
+		System.out.println("=============================");
+		System.out.println();
+		System.out.println("Search");
+		System.out.println("Search via a movie or cineplex");
+		System.out.println();
+		System.out.println("=============================");
+		System.out.println();
+		System.out.println("1. Search movie");
+		System.out.println("2. Search cineplex");
+		System.out.println();	
+		System.out.println("0. Back");
+		System.out.println();
+		System.out.println("=============================");
+		System.out.println("Choose an option (0-2):");
 	}
 
 	@Override
@@ -23,7 +36,17 @@ public class SearchMenu implements MenuInterface {
 		 * Here will be how the system responds to user input, and thus correct and incorrect responses, in conjunction with the
 		 * displayMenu(); function.
 		 */
-		
+		String input = null;
+		do {
+			try {
+				BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+				input = reader.readLine();
+				if(input.equals("1")) {
+					searchMovie();
+				}
+				reader.close();
+			} catch (IOException e) {}
+		} while(input == "0");
 	}
 	
 	/*
@@ -39,7 +62,56 @@ public class SearchMenu implements MenuInterface {
 
 	public MovieSession searchMovieName(String movieName)
 	{
+		int matchCount = 0;
+		ArrayList<MovieSession> movieSessions = jMossClient.data.getMovieSessions();
+		for(int i = 0; i < movieSessions.size(); i++) {
+			if(movieSessions.get(i).getMovieName().equals(movieName)) {
+				System.out.println(movieSessions.get(i).getAsFormattedString());
+				matchCount++;
+			}
+		}
+		if(matchCount == 0) {
+			System.out.println("No Results Found");
+		}
 		return null;
+		return null;
+	}
+	
+	public void searchMovie() {
+		String title = null;
+		System.out.println("=============================");
+		System.out.println();
+		System.out.println("Search");
+		System.out.println("Search movie");
+		System.out.println();
+		System.out.println("=============================");
+		System.out.println();
+		System.out.println("Please enter a movie title (0 to go back): ");
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			while((title = reader.readLine()) != null){
+				if(title.equals("0")) {
+					displayMenu();
+					break;
+				}
+				searchMovieName(title);
+				searchMovie();
+			}
+			reader.close();
+		} catch (IOException e) {}
+		System.out.println();
+	}
+	
+	public void searchCineplex() {
+		System.out.println("=============================");
+		System.out.println();
+		System.out.println("Search");
+		System.out.println("Search cineplex");
+		System.out.println();
+		System.out.println("=============================");
+		System.out.println();
+		System.out.println("Please enter a location (0 to go back): ");
+		System.out.println();
 	}
 
 }
