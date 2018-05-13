b0cc00ef9426c108c87f75020fd927b7998d0391
Drkjay <31405711+Drkjay@users.noreply.github.com>
2018-05-13 18:22:54 +1000

    Update BookingMenu.java
    Added some basic comments, will go into detail later.

--- jMoSS/jMoSS/src/model/menus/BookingMenu.java
+++ jMoSS/jMoSS/src/model/menus/BookingMenu.java
@@ -11,14 +11,17 @@ import java.util.*;
 
 public class BookingMenu implements MenuInterface {
 
+	// A String array to keep hold of the options that a User has entered.
 	private String[] options = new String[4];
 
 	@Override
 	public void displayMenu() {
 		System.out.println("=============================");
 		System.out.println("Book A Movie Session");
+		// Prints the status of the Booking.
 		System.out.println(getBookingProgress());
 		System.out.println("=============================");
+		// Prints each option, where the wording can be changed if an option has already been chosen.
 		System.out.println("1. Choose a" + showDifference(0) + " Cineplex." + showCurrent(0));
 		System.out.println("2. Choose a" + showDifference(1) + " Movie." + showCurrent(1));
 		System.out.println("3. Choose a" + showDifference(2) + " Session." + showCurrent(2));
@@ -53,7 +56,7 @@ public class BookingMenu implements MenuInterface {
 			case 5:
 				// Return Data \ Make Booking
 				if (countNonInputs() == 0)
-					// Here you would do 3.9 & 4.3 - returning variables of the options array.
+					// Here you would do 3.9 & 4.3 - create a booking with the variables of the options array.
 					exit = true;
 				else
 					System.out.println("You must satisfy every option before confirming the Booking!");
@@ -63,7 +66,9 @@ public class BookingMenu implements MenuInterface {
 
 	}
 
+	// Menu for choosing a Cinemaplex.
 	private String chooseCineplex() {
+		// NOTE: No file exists for a list of Cinemaplex's? I think we should make one and refactor this.
 		System.out.println("=============================");
 		System.out.println("Book A Movie Session");
 		System.out.println("Choose a Cineplex");
@@ -98,6 +103,7 @@ public class BookingMenu implements MenuInterface {
 		System.out.println("Book A Movie Session");
 		System.out.println("Choose a Movie");
 		System.out.println("=============================");
+		// Print each of the movies with an added number and '.'
 		int id = 1;
 		List<String> movieList = getMovieList();
 		for (String movie : movieList) {
@@ -106,10 +112,13 @@ public class BookingMenu implements MenuInterface {
 		}
 		System.out.println("=============================");
 		
+		// Dynamically set the amount of options (0 - x) allowed depending on how many movies there are.
 		int movieID = getOptionInput(0, movieList.size());
+		// User wants to go back, return null
 		if (movieID == 0)
 			return null;
 		else {
+			// Iterate through each Movie until it gets to the selection id, then return it.
 			Iterator<String> movieIterator = movieList.iterator();
 			int currentMovieID = 1;
 			String selection = "";
@@ -121,14 +130,17 @@ public class BookingMenu implements MenuInterface {
 		}
 	}
 
+	// To be implemented when we implement session time.
 	private String chooseSession() {
 		return null;
 	}
 
+	// To be implemented when we implement Number of People.
 	private String chooseNumberOfPeople() {
 		return null;
 	}
 
+	// Returns an empty string or "different" for the wording of the booking menu.
 	private String showDifference(int optionID) {
 		if (options[optionID] == null)
 			return "";
@@ -136,6 +148,7 @@ public class BookingMenu implements MenuInterface {
 			return " different";
 	}
 
+	// Returns an empty string or the currently chosen option for each line in the booking menu.
 	private String showCurrent(int optionID) {
 		if (options[optionID] == null)
 			return "";
@@ -143,6 +156,7 @@ public class BookingMenu implements MenuInterface {
 			return " (Current: \"" + options[optionID] + "\")";
 	}
 
+	// Returns the name of each option by an id starting at 0.
 	private String getOptionNameByID(int optionID) {
 		if (optionID == 0)
 			return "Cineplex";
@@ -156,6 +170,7 @@ public class BookingMenu implements MenuInterface {
 			return "UKNOWN_OPTION_NAME";
 	}
 
+	// Returns how many options have not been allocated a value.
 	private int countNonInputs() {
 		int totalNonInputs = 0;
 		for (int i = 0; (i < options.length); i++) {
@@ -169,11 +184,14 @@ public class BookingMenu implements MenuInterface {
 
 		int totalNonInputs = countNonInputs();
 
+		// No options have been given a value.
 		if (totalNonInputs == options.length)
 			return "Make a booking by choosing a Cineplex, Movie, Session Time and Number of people.";
+		// All options have been given a value
 		else if (totalNonInputs == 0)
 			return "All options have been entered. Confirm the booking to continue.";
 		else {
+			// The following options need to be given a value...
 			StringBuilder nonCompletedOptions = new StringBuilder("Continue the Booking process by entering a: | ");
 			for (int i = 0; (i < options.length); i++) {
 				if (options[i] == null)
@@ -183,6 +201,9 @@ public class BookingMenu implements MenuInterface {
 		}
 	}
 
+	// NOTE: the Scanner is not closed, because other classes use Scanner and closing it here makes it unusable again.
+	// NOTE2: I highly suggest that this function is made available to everyone, as it will allow users to only enter numbers
+	// Between a set min and max value. It's very useful across our entire application.
 	@SuppressWarnings("resource")
 	private int getOptionInput(int min, int max) {
 
@@ -214,12 +235,15 @@ public class BookingMenu implements MenuInterface {
 	private List<String> getMovieList() {
 		List<String> movieList = new ArrayList<String>();
 		try {
+			// Iterate through each line in the file.
 			FileReader fileReader = new FileReader("MovieSession.txt");
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 			String line;
 			while ((line = bufferedReader.readLine()) != null) {
+				// Break it up wherever there is a '/'
 				String[] tokens = line.split("\\/");
 				boolean alreadyExists = false;
+				// Go through the movieList and add the movie if its not already in the list.
 				if (movieList != null) {
 					for (String movie : movieList) {
 						if (movie.equals(tokens[1])) {
@@ -230,6 +254,7 @@ public class BookingMenu implements MenuInterface {
 					if (!alreadyExists)
 						movieList.add(tokens[1]);
 				} else
+					// This is the first movie, so add it without doing check.
 					movieList.add(tokens[1]);
 			}
 			bufferedReader.close();
